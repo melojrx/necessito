467bce2c540d957a131a3928b838ee80d3a385b7
MeloJR <jrmeloafrf@gmail.com>
2025-01-27 22:16:35 -0300

    inclusao do CRUD de orcamentos

--- budgets/urls.py
+++ budgets/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import SubmeterOrcamentoView
+from .views import SubmeterOrcamentoView, budgetListView, budgetDetailView, BudgetUpdateView, budgetDeleteView
 
 urlpatterns = [
     path('submeter/<int:pk>/', SubmeterOrcamentoView.as_view(), name='submeter_orcamento'),
+    path('budgets/', budgetListView.as_view(), name='budget_list'),
+    path('budgets/<int:pk>/', budgetDetailView.as_view(), name='budget_detail'),
+    path('budgets/<int:pk>/editar/', BudgetUpdateView.as_view(), name='budget_update'),
+    path('budgets/<int:pk>/excluir/', budgetDeleteView.as_view(), name='budget_delete'),
 ]
--- budgets/views.py
+++ budgets/views.py
@@ -1,4 +1,5 @@
-from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, UpdateView, DeleteView, ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
@@ -34,3 +35,62 @@ class SubmeterOrcamentoView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         # Redireciona de volta para os detalhes do anúncio
         return self.object.anuncio.get_absolute_url()
+
+class budgetListView(LoginRequiredMixin, ListView):
+    model = Orcamento
+    template_name = 'budget_list.html'
+    context_object_name = 'orcamentos'
+
+    def get_queryset(self):
+        # Filtra os orçamentos pelo fornecedor logado
+        queryset = Orcamento.objects.filter(fornecedor=self.request.user).order_by('-data_criacao')
+
+        # Aplica filtro de descrição, se fornecido
+        search_query = self.request.GET.get('search', None)
+        if search_query:
+            queryset = queryset.filter(descricao__icontains=search_query)
+        
+        # Aplica filtro de status, se fornecido
+        status_filter = self.request.GET.get('status', None)
+        if status_filter:
+            queryset = queryset.filter(status=status_filter)
+        
+        return queryset
+
+class BudgetUpdateView(LoginRequiredMixin, UpdateView):
+    model = Orcamento
+    form_class = OrcamentoForm
+    template_name = 'budget_update.html'
+    success_url = reverse_lazy('budget_list')  # URL da lista de orçamentos
+
+    def get_queryset(self):
+        # Filtra os orçamentos pelo fornecedor logado
+        return Orcamento.objects.filter(fornecedor=self.request.user)
+
+    def form_valid(self, form):
+        # Adiciona uma mensagem de sucesso
+        messages.success(self.request, "Orçamento atualizado com sucesso!")
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        # Retorna para a lista de orçamentos
+        return reverse_lazy('budget_list')
+
+class budgetDetailView(LoginRequiredMixin, DetailView):
+    model = Orcamento
+    template_name = 'budget_detail.html'
+    context_object_name = 'orcamento'
+
+class budgetDeleteView(LoginRequiredMixin, DeleteView):
+    model = Orcamento
+    template_name = 'budget_delete.html'
+    success_url = reverse_lazy('budget_list')
+
+    def get_queryset(self):
+        # Filtra os orçamentos pelo fornecedor logado
+        queryset = Orcamento.objects.filter(fornecedor=self.request.user)
+        return queryset
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "Orçamento excluído com sucesso!")
+        return super().delete(request, *args, **kwargs)

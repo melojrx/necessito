db2c9c0ec7ac05bc7f41e3bc1c58ce4d716955ea
MeloJR <jrmeloafrf@gmail.com>
2025-05-17 16:23:52 -0300

    Ajustes de dev

--- core/settings/base.py
+++ core/settings/base.py
@@ -159,8 +159,6 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

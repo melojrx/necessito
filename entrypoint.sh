cc2ad112a89982e65f07daef020b61c1ecc8987b
melojrx <jrmeloafrf@gmail.com>
2025-06-10 17:40:57 -0300

    ajuste de

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Aguarda o banco de dados estar pronto
-echo "Aguardando banco de dados..."
-while ! nc -z db 5432; do
-  sleep 0.1
-done
-
-echo "Banco de dados conectado!"
-
-# Executa migrações
-echo "Executando migrações..."
-python manage.py migrate --noinput
-
 # Coleta arquivos estáticos
 echo "Coletando arquivos estáticos..."
 python manage.py collectstatic --noinput
